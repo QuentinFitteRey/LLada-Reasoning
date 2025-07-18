fixing grpo and chat template, debugging gradient flow

dpo_trainer.py
@@ -5,7 +5,7 @@ import torch
 from torch.optim import Optimizer
 from tqdm import tqdm
 import torch.nn.functional as F
-from openrlhf.models import DPOLoss
+from DPOLoss import DPOLoss
 from openrlhf.utils.distributed_sampler import DistributedSampler
 
 
@@ -161,12 +161,17 @@ class DPOTrainer(ABC):
                 chosen_logps, chosen_shared = self.model(chosen_ids, c_mask, mode="monte_carlo")
                 rejected_logps, rejected_shared = self.model(reject_ids, r_mask, mode="monte_carlo")
                 with torch.no_grad():
-                    reference_chosen_logps, _ = self.ref_model(chosen_ids, c_mask, shared_mask=chosen_shared, mode="monte_carlo")
-                    reference_rejected_logps, _ = self.ref_model(reject_ids, r_mask, shared_mask=rejected_shared, mode="monte_carlo")
+                    if step < 150:
+                        # use monte carlo sampling for the first 150 steps
+                        reference_chosen_logps, _ = self.ref_model(chosen_ids, c_mask, mode="monte_carlo")
+                        reference_rejected_logps, _ = self.ref_model(reject_ids, r_mask, mode="monte_carlo")
+                    else:
+                        reference_chosen_logps, _ = self.ref_model(chosen_ids, c_mask, shared_mask=chosen_shared, mode="monte_carlo")
+                        reference_rejected_logps, _ = self.ref_model(reject_ids, r_mask, shared_mask=rejected_shared, mode="monte_carlo")
 
                 # loss function
                 preference_loss, chosen_reward, reject_reward = self.loss_fn(
-                    -chosen_logps, -rejected_logps, -reference_chosen_logps, -reference_rejected_logps
+                    chosen_logps, rejected_logps, reference_chosen_logps, reference_rejected_logps
                 )
                 # mixtral
                 if not self.aux_loss:
@@ -178,7 +183,9 @@ class DPOTrainer(ABC):
                 loss = preference_loss + aux_loss * self.args.aux_loss_coef + nll_loss * self.args.nll_loss_coef
                 self.strategy.backward(loss, self.model, self.optimizer)
                 self.strategy.optimizer_step(self.optimizer, self.model, self.scheduler)
-
+                for name, param in self.model.named_parameters():
+                    if param.grad is not None:
+                        print("gradients: ", name, param.grad.abs().mean())
                 acc = (chosen_reward > reject_reward).float().mean().item()
                 acc_sum += acc
                 loss_sum += preference_loss.item()
@@ -270,7 +277,7 @@ class DPOTrainer(ABC):
                 reference_rejected_logps, _ = self.ref_model(reject_ids, r_mask, shared_mask=rejected_shared, mode="monte_carlo")
 
                 loss, chosen_reward, reject_reward = self.loss_fn(
-                    -chosen_logps, -rejected_logps, -reference_chosen_logps, -reference_rejected_logps
+                    chosen_logps, rejected_logps, reference_chosen_logps, reference_rejected_logps
                 )
                 acc_sum += (chosen_reward > reject_reward).float().mean().item()
                 loss_sum += loss.item()

llada_actor.py
@@ -145,10 +145,10 @@ class LLadaActor(nn.Module):
         """Returns action log probs"""
         batch, seqlen = sequences.size()
         if mode == "monte_carlo":
-            output = self.monte_carlo_forward(sequences, attention_mask)
+            output = self.monte_carlo_forward(sequences, attention_mask, shared_mask=shared_mask)
             return output if not return_output else (output, {"logits": None})  # logits not used in MC
         elif mode == "fall_through":
-            return self.model(sequences, attention_mask, shared_mask=shared_mask)
+            return self.model(sequences, attention_mask)
         else:
             """
             Really not sure here what correspond to the original autoregressive terms. Just returning some analogous
@@ -192,6 +192,7 @@ class LLadaActor(nn.Module):
         """
         Compute the ELBO estimator with double monte carlo
         """
+        # print("This is one run")
         b = input_ids.size(0)
         total_loss = torch.zeros(b, device=input_ids.device)
         if shared_mask is None:
@@ -206,6 +207,7 @@ class LLadaActor(nn.Module):
                     if l_pi is not None:
                         total_loss += l_pi  # loss is shape (B,)
                         count += 1
+                    # print(f"Random sampling: t:{t}; mask:{masked.sum()}; l_pi:{l_pi}")
             if count == 0:
                 return torch.zeros(b, device=input_ids.device)
             return total_loss / count, shared_mask  # shape: (B,)
@@ -215,6 +217,7 @@ class LLadaActor(nn.Module):
                 l_pi, _ = self.calc_loss(input_ids, input_mask, noisy_input, masked, t)
                 if l_pi is not None:
                     total_loss += l_pi
+                # print(f"shared mask: t:{t}; mask:{masked.sum()}; l_pi:{l_pi}")
             return total_loss / len(shared_mask), shared_mask  # shape: (B,)
 
     def calc_loss(self, input_ids, attention_mask, noisy_input, masked, t):